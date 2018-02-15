7a6f702738162dba1398bda4b6eb5778d67cc306
Florian Rüberg <florian.rueberg@gmail.com>
2018-02-15 22:30:09 +0100

    Add bootstrap example and fix animation offset

--- packages/anyform-react/src/index.js
+++ packages/anyform-react/src/index.js
@@ -1,6 +1,66 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
+import { Button } from 'reactstrap';
+
+let Example = (props) => {
+    return <div class="container">
+        <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Project name</h3>
+        </div>
+
+        <div class="jumbotron">
+        <h1 class="display-3">Jumbotron heading</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+        </div>
+
+        <div class="row marketing">
+        <div class="col-lg-6">
+            <h4>Subheading</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+            <h4>Subheading</h4>
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+            <h4>Subheading</h4>
+            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+            <h4>Subheading</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+            <h4>Subheading</h4>
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+            <h4>Subheading</h4>
+            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+        </div>
+
+        <footer class="footer">
+        <p>&copy; Company 2017</p>
+        </footer>
+
+    </div>;
+};
+
 var easing = {
     // no easing, no acceleration
     linear: function (t) { return t },
@@ -39,9 +99,9 @@ export class AnimatedRoot extends Component {
         this.state = {time: 0, epoch: 0, startms: 0};
     }
 
-    render = () => <AnimatedNodeList 
+    render = () => [<Example />,<AnimatedNodeList 
             nodes={this.props.nodes} root={this}
-            epoch={this.state.epoch} time={this.state.time} />;
+            epoch={this.state.epoch} time={this.state.time} />];
 
     componentWillReceiveProps(nextProps) {
         // Start animation countdown
@@ -127,7 +187,7 @@ class Animated extends Component {
 
     recieveBoundingRect(wrapper) {
         if (!wrapper) return;
-        let to = wrapper.getBoundingClientRect();
+        let to = { x: wrapper.offsetLeft, y: wrapper.offsetTop };
 
         if (this.props.time === 0 || !this.state.from) {
             if (this.state.offset || !this.state.from) {
