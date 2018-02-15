dcb32e60aaa7e784fcbc16e24ea9ba213ac7c585
Florian Rüberg <florian.rueberg@gmail.com>
2018-02-15 17:10:46 +0100

    First smooth animation

--- docs/react-hello-world/index.js
+++ docs/react-hello-world/index.js
@@ -11,8 +11,13 @@ class Example extends Component {
             data: [
                 {id: 1, value: '1'},
                 {id: 2, value: '2'},
-                {id: 3, value: '3'},
-                {id: 4, value: '4'},
+                {id: 3, value: '3', contains: [
+                    {id: 4, value: '4'},
+                    {id: 5, value: '5'},
+                    {id: 6, value: '6'},
+                    {id: 7, value: '7'},
+                ]}, 
+                {id: 8, value: '8'},
             ]
         };
     }
@@ -24,8 +29,9 @@ class Example extends Component {
         </div>;
     }
 
-    shuffle(array) {
+    shuffle() {
         var array = this.state.data.slice();
+                
         var currentIndex = array.length, temporaryValue, randomIndex;
       
         // While there remain elements to shuffle...
--- packages/anyform-react/src/index.js
+++ packages/anyform-react/src/index.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+
+var easing = {
+    // no easing, no acceleration
+    linear: function (t) { return t },
+    // accelerating from zero velocity
+    easeInQuad: function (t) { return t*t },
+    // decelerating to zero velocity
+    easeOutQuad: function (t) { return t*(2-t) },
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+    // accelerating from zero velocity 
+    easeInCubic: function (t) { return t*t*t },
+    // decelerating to zero velocity 
+    easeOutCubic: function (t) { return (--t)*t*t+1 },
+    // acceleration until halfway, then deceleration 
+    easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+    // accelerating from zero velocity 
+    easeInQuart: function (t) { return t*t*t*t },
+    // decelerating to zero velocity 
+    easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+    // accelerating from zero velocity
+    easeInQuint: function (t) { return t*t*t*t*t },
+    // decelerating to zero velocity
+    easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+    // acceleration until halfway, then deceleration 
+    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+};
+
+
 export class AnimatedRoot extends Component {
 
     constructor(props) {
         super(props);
         // TODO add current, stop jumps
-        this.state = {time: 0, before: {}, target: {}};
+        this.state = {time: 0, startms: 0, lengthms: 300, before: {}, target: {}};
+        this.continueCountdown();
     }
 
     render() {
@@ -17,9 +49,10 @@ export class AnimatedRoot extends Component {
         // TODO calculate from state
         if (!this.state.before[id] || !this.state.target[id]) return {left: 0, top: 0};
 
+        let factor = easing.easeInOutCubic(this.state.time / this.state.lengthms);
         return {
-            left: (this.state.before[id].x - this.state.target[id].x) * this.state.time,
-            top: (this.state.before[id].y - this.state.target[id].y) * this.state.time,
+            left: (this.state.before[id].x - this.state.target[id].x) * factor,
+            top: (this.state.before[id].y - this.state.target[id].y) * factor,
         }
     }
 
@@ -29,38 +62,56 @@ export class AnimatedRoot extends Component {
 
     componentWillReceiveProps(nextProps) {
         // Move current rects to leagcy and start state countdown
-        this.state.before = Object.assign({}, this.state.target);
-        this.state.time = 1.0;
-        this.continueCountdown();
+        this.setState({
+            before: Object.assign({}, this.state.target),
+            time: 1.0,
+            startms: Date.now()
+        });
     }
 
     continueCountdown() {
-        if (this.state.time <= 0.0) return;
-        // TODO replace mock animation
-        let decremented = this.state.time - 0.05;
-        this.setState({time: decremented});
-        window.requestAnimationFrame(() => this.continueCountdown());
+        window.requestAnimationFrame(() => {
+            if (this.state.time > 0.0) {
+                
+                let dif = Date.now() - this.state.startms;
+                this.setState({time: Math.max(0, this.state.lengthms - dif)});
+            }
+
+            this.continueCountdown()
+        });
     }
 }
 
 class AnimatedNodeList extends Component {
 
     render() {
-        let {nodes} = this.props;
-        return nodes.map((node) => <Animated key={node.id} id={node.id} root={this.props.root} time={this.props.time}>
-            {this.renderNode(node)}
-        </Animated>);
+        let { nodes, ...props } = this.props;
+        return nodes.map((node) => this.renderNode(props, node));
     }
 
-    renderNode(node) {
-        // TODO: render AnimatedNodeList and pass params to: multi, contains
-        return <input value={node.value} />;
+    getAnimated(props, node) {
+        let id = this.getId(node);
+        return <Animated key={id} id={id} {...props}><input value={node.value} /></Animated>;
+    }
+
+    getId = (node) => node.id;
+
+    renderNode(props, node) {
+        var input = this.getAnimated(props, node);
+        
+        if (!node.contains) return input;
+        let children = <div style={{marginLeft: '20px'}}>
+            <AnimatedNodeList {...this.props} nodes={node.contains}  />
+        </div>;
+        return [input, children];
     }
 }
 
 class Animated extends Component {
     render() {
-        return <div ref="wrapper" style={this.getStyle()}>{this.props.children}</div>;
+        return <div ref="wrapper">
+            <div style={this.getStyle()}>{this.props.children}</div>
+        </div>;
     }
 
     getStyle() {
@@ -69,8 +120,12 @@ class Animated extends Component {
             ...this.props.root.getStyle(this.props.id)
         }
     }
-    componentDidUpdate(prevProps) {
+
+    notifyRootOfPosition() {
         let wrapper = ReactDOM.findDOMNode(this.refs.wrapper);
-        this.props.root.updateRect(this.props.id, wrapper.getBoundingClientRect());        
+        this.props.root.updateRect(this.props.id, wrapper.getBoundingClientRect());
     }
+
+    componentDidUpdate = () => this.notifyRootOfPosition()
+    componentDidMount = () => this.notifyRootOfPosition()
 }
